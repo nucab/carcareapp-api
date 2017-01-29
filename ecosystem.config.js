00218c426ef08518f17b21031a6c182dd0ef4426
Noah John Ucab <noahjohn.ucab@gmail.com>
2017-01-29 21:00:14 +0800

    commit

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "carcareapp-api",
+      script    : "server.js",
+      env_production : {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "carcareapp",
+      host : "138.197.108.171",
+      ref  : "origin/master",
+      repo : "https://github.com/noahjohn9259/carcareapp-api.git",
+      path : "/home/carcareapp",
+      "post-deploy" : "nvm install && npm install && /home/carcareapp/.nvm/versions/node/v6.9.4/bin/pm2 startOrRestart ~/source/ecosystem.config.js --env production"
+    }
+  }
+}
