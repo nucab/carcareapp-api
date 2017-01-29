5f48cc02480ad5b3a5ee1d40535e62949eca95b2
Noah John Ucab <noahjohn.ucab@gmail.com>
2017-01-30 07:52:58 +0800

    asd

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-require("express")()
-  .get("/", (req, res, next) => {
-    console.log("request received", Date.now())
-    res.send("hey world")
-  })
-  .listen(3000)
+const express = require('express');
+const bodyParser = require('body-parser')
+const knex = require('knex');
+
+const config = require('./knexfile')[process.env.NODE_ENV || 'development'];
+
+const app = express();
+
+const db = knex(config);
+
+app.use(bodyParser.json());
+app.get('/api/users', (req, res, next) => {
+	db('users').then((users) => {
+		res.send(users);
+	}).catch(err => {
+		res.send(err);
+	});
+});
+app.listen(3000);
