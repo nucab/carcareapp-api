563868ab46062656552227552dca599a66fb00c2
Noah John Ucab <noahjohn.ucab@gmail.com>
2017-01-30 14:50:14 +0800

    update config

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,36 @@
 // Update with your config settings.
 
 module.exports = {
-
-	development: {
-		client: 'mysql',
-		connection: {
-			host: '127.0.0.1',
-			database: 'abc_carmanagement',
-			user:     'root'
-		}
-	},
-
-	staging: {
-		client: 'postgresql',
-		connection: {
-			database: 'my_db',
-			user:     'username',
-			password: 'password'
-		},
-		pool: {
-			min: 2,
-			max: 10
-		},
-		migrations: {
-			tableName: 'knex_migrations'
-		}
-	},
-
-	production: {
-		client: 'mysql',
-		connection: {
-			host: 'localhost',
-			database: 'carcareapp_api',
-			user:     'carcareapp_user',
-			password: 'fa0b579a'
-		},
-		pool: {
-			min: 2,
-			max: 10
-		},
-		migrations: {
-			tableName: 'knex_migrations'
-		}
-	}
-
+  development: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      database: 'abc_carmanagement',
+      user:     'root'
+    }
+  },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  production: {
+    client: 'mysql',
+    connection: {
+      host: 'localhost',
+      database: 'carcareapp_api',
+      user:     'carcareapp_user',
+      password: 'yA6#NPq34?L6yNnj'
+    }
+  }
 };
